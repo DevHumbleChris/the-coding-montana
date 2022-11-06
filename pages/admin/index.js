c692e6eb8245a0545a52759dcddf939b4ef6dfb1
DevHumbleChris <dev.humblechris254@gmail.com>
2022-11-06 11:59:05 +0300

    Added Admin Area

---
+++ pages/admin/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function index() {
+  return (
+    <main>
+      <Head>
+        <title>Admin Area - The Coding Montana</title>
+        <meta name="description" content="The Coding Montana - Portfolio" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </main>
+  )
+}
