587064501f9a3d229231cdbc7ae420392644a22a
DevHumbleChris <dev.humblechris254@gmail.com>
2022-11-07 11:51:26 +0300

    Layout Fixed

--- components/AdminHeader.jsx
+++ components/AdminHeader.jsx
@@ -6,6 +6,7 @@ import {
   BookmarkIcon,
   BriefcaseIcon
 } from "@heroicons/react/24/outline";
+import Link from "next/link";
 
 export default function AdminHeader() {
   return (
@@ -22,22 +23,30 @@ export default function AdminHeader() {
           <h1 className="text-center">Coding Montana</h1>
         </div>
         <div className="flex flex-col space-y-6 mt-8">
-          <button className="flex space-x-2 items-center hover:border-l-2 hover:border-[#514fa1] hover:px-2 hover:text-gray-400">
+          <Link href="/admin/dashboard">
+          <a className="flex space-x-2 items-center hover:border-l-2 hover:border-[#514fa1] hover:px-2 hover:text-gray-400">
             <HomeModernIcon className="w-7 h-7 mx-auto md:mx-0" />
             <h2 className="hidden md:inline-block">Dashboard</h2>
-          </button>
-          <button className="flex space-x-2 items-center hover:border-l-2 hover:border-[#514fa1] hover:px-2 hover:text-gray-400">
-            <BriefcaseIcon className="w-7 h-7 mx-auto md:mx-0" />
-            <h2 className="hidden md:inline-block">Projects</h2>
-          </button>
-          <button className="flex space-x-2 items-center hover:border-l-2 hover:border-[#514fa1] hover:px-2 hover:text-gray-400">
-            <PencilSquareIcon className="w-7 h-7 mx-auto md:mx-0" />
-            <h2 className="hidden md:inline-block">Articles</h2>
-          </button>
-          <button className="flex space-x-2 items-center hover:border-l-2 hover:border-[#514fa1] hover:px-2 hover:text-gray-400">
-            <BookmarkIcon className="w-7 h-7 mx-auto md:mx-0" />
-            <h2 className="hidden md:inline-block">Tasks</h2>
-          </button>
+          </a>
+          </Link>
+          <Link href="/admin/projects">
+            <a className="flex space-x-2 items-center hover:border-l-2 hover:border-[#514fa1] hover:px-2 hover:text-gray-400">
+              <BriefcaseIcon className="w-7 h-7 mx-auto md:mx-0" />
+              <h2 className="hidden md:inline-block">Projects</h2>
+            </a>
+          </Link>
+          <Link href="/admin/articles">
+            <a className="flex space-x-2 items-center hover:border-l-2 hover:border-[#514fa1] hover:px-2 hover:text-gray-400">
+              <PencilSquareIcon className="w-7 h-7 mx-auto md:mx-0" />
+              <h2 className="hidden md:inline-block">Articles</h2>
+            </a>
+          </Link>
+          <Link href="/admin/tasks">
+            <a className="flex space-x-2 items-center hover:border-l-2 hover:border-[#514fa1] hover:px-2 hover:text-gray-400">
+              <BookmarkIcon className="w-7 h-7 mx-auto md:mx-0" />
+              <h2 className="hidden md:inline-block">Tasks</h2>
+            </a>
+          </Link>
         </div>
       </div>
       <button className="absolute bottom-0 m-2 hover:border-l-2 right-0 left-0 flex items-center space-x-2 hover:border-[#514fa1] hover:px-2 hover:text-gray-400">
