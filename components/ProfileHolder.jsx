d7f02c9fd107ec374f366e32e3f916f95585b4a1
DevHumbleChris <dev.humblechris254@gmail.com>
2022-11-17 17:12:38 +0300

    reversed everything

--- components/ProfileHolder.jsx
+++ components/ProfileHolder.jsx
@@ -1,12 +1,8 @@
 import React from "react";
-import { motion } from "framer-motion";
 
 function ProfileHolder() {
   return (
-    <motion.svg
-      initial={{ x: -500, opacity: 0, scale: 1.5 }}
-      whileInView={{ x: 0, opacity: 1, scale: 1 }}
-      transition={{ duration: 1.0 }}
+    <svg
       xmlns="http://www.w3.org/2000/svg"
       xmlnsXlink="http://www.w3.org/1999/xlink"
       x="0"
@@ -9502,7 +9498,7 @@ function ProfileHolder() {
           </g>
         </g>
       </g>
-    </motion.svg>
+    </svg>
   );
 }
 
