5955929e6645efed697b83b0f9c77111af9073e6
DevHumbleChris <dev.humblechris254@gmail.com>
2022-11-22 14:23:15 +0300

    Added MongoDB

--- pages/admin/auth/signin.jsx
+++ pages/admin/auth/signin.jsx
@@ -1,77 +1,72 @@
-import { LockClosedIcon } from '@heroicons/react/20/solid'
-import Image from 'next/image'
+import { LockClosedIcon } from "@heroicons/react/20/solid";
+import Image from "next/image";
 import Head from "next/head";
 
 export default function Example() {
   return (
     <>
-    <Head>
+      <Head>
         <title>Admin Login - The Coding Montana</title>
         <meta name="description" content="The Coding Montana - Portfolio" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-        <div className="w-full max-w-md p-2 space-y-8">
-          <div>
-            <div className='w-60 h-60 mx-auto'>
-              <Image
-                height={1080}
-                width={1080}
-                alt="the-coding-montana"
-                src="/images/the-coding-montana.png"
-              />
-            </div>
-            <h2 className="-mt-8 text-center text-3xl font-bold tracking-tight text-gray-900">
-              Administration Login
-            </h2>
-          </div>
-          <form className="-mt-8 space-y-6" action="#" method="POST">
-            <input type="hidden" name="remember" defaultValue="true" />
-            <div className="-space-y-px rounded-md shadow-sm">
-              <div>
-                <label htmlFor="email-address" className="sr-only">
-                  Email address
-                </label>
-                <input
-                  id="email-address"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  required
-                  className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                  placeholder="Email address"
-                />
-              </div>
-              <div>
-                <label htmlFor="password" className="sr-only">
-                  Password
-                </label>
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
-                  required
-                  className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                  placeholder="Password"
-                />
-              </div>
-            </div>
+      <div className="relative py-16">
+        <div className="container relative m-auto px-6 text-gray-500 md:px-12 xl:px-40">
+          <div className="m-auto space-y-6 md:w-8/12 lg:w-6/12 xl:w-6/12">
+            <div className="rounded-3xl border border-gray-100 bg-white dark:bg-gray-800 dark:border-gray-700 shadow-2xl shadow-gray-600/10 backdrop-blur-2xl">
+              <div className="p-8 py-12 sm:p-16">
+                <h2 className="mb-8 text-2xl font-bold text-gray-800">
+                  Sign in to your account
+                </h2>
+                <form action="" className="space-y-8">
+                  <div className="space-y-2">
+                    <label
+                      for="email"
+                      className="text-gray-60"
+                    >
+                      Email
+                    </label>
+                    <input
+                      type="email"
+                      name="email"
+                      id="email"
+                      autocomplete="username"
+                      className="focus:outline-none block w-full rounded-md border border-gray-200 bg-transparent px-4 py-3 text-gray-600 transition duration-300 invalid:ring-2 invalid:ring-red-400 focus:ring-2 focus:ring-cyan-300"
+                    />
+                  </div>
 
-            <div>
-              <button
-                type="submit"
-                className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-              >
-                <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                  <LockClosedIcon className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
-                </span>
-                Sign in
-              </button>
+                  <div>
+                    <div className="flex items-center justify-between">
+                      <label
+                        for="pwd"
+                        className="text-gray-600"
+                      >
+                        Password
+                      </label>
+                    </div>
+                    <input
+                      type="password"
+                      name="pwd"
+                      id="pwd"
+                      autocomplete="current-password"
+                      className="focus:outline-none block w-full rounded-md border border-gray-200 bg-transparent px-4 py-3 text-gray-600 transition duration-300 invalid:ring-2 invalid:ring-red-400 focus:ring-2 focus:ring-cyan-300"
+                    />
+                  </div>
+
+                  <button
+                    type="submit"
+                    className="relative flex h-11 w-full items-center justify-center px-6 before:absolute before:inset-0 before:rounded-full before:bg-primary before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95"
+                  >
+                    <span className="relative text-base font-semibold text-white">
+                      Connect
+                    </span>
+                  </button>
+                </form>
+              </div>
             </div>
-          </form>
+          </div>
         </div>
       </div>
     </>
-  )
+  );
 }
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,10 +1,13 @@
 import NextAuth from 'next-auth'
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
+import clientPromise from "../../../lib/mongodb"
 
 export const authOptions = {
     providers: [],
     pages: {
         signIn: '/admin/auth/signin'
-    }
+    },
+    adapter: MongoDBAdapter(clientPromise),
 }
 
 export default NextAuth(authOptions)
