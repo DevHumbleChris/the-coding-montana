387e8e0cb4fec3f7652f7341ee05936d7d4f1f44
DevHumbleChris <dev.humblechris254@gmail.com>
2022-11-22 16:33:22 +0300

    ZAdded Credentials Provider

--- pages/admin/auth/signin.jsx
+++ pages/admin/auth/signin.jsx
@@ -1,8 +1,20 @@
-import { LockClosedIcon } from "@heroicons/react/20/solid";
-import Image from "next/image";
 import Head from "next/head";
+import { signIn } from "next-auth/react";
+import { useState } from "react";
 
 export default function Example() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await signIn("credentials", {
+      email,
+      password,
+      redirect: false
+    });
+    console.log(response);
+  };
   return (
     <>
       <Head>
@@ -13,34 +25,30 @@ export default function Example() {
       <div className="relative py-16">
         <div className="container relative m-auto px-6 text-gray-500 md:px-12 xl:px-40">
           <div className="m-auto space-y-6 md:w-8/12 lg:w-6/12 xl:w-6/12">
-            <div className="rounded-3xl border border-gray-100 bg-white dark:bg-gray-800 dark:border-gray-700 shadow-2xl shadow-gray-600/10 backdrop-blur-2xl">
+            <div className="rounded-3xl border border-gray-100 bg-white shadow-2xl shadow-gray-600/10 backdrop-blur-2xl">
               <div className="p-8 py-12 sm:p-16">
                 <h2 className="mb-8 text-2xl font-bold text-gray-800">
                   Sign in to your account
                 </h2>
                 <form action="" className="space-y-8">
                   <div className="space-y-2">
-                    <label
-                      for="email"
-                      className="text-gray-60"
-                    >
+                    <label htmlFor="email" className="text-gray-60">
                       Email
                     </label>
                     <input
                       type="email"
                       name="email"
                       id="email"
-                      autocomplete="username"
-                      className="focus:outline-none block w-full rounded-md border border-gray-200 bg-transparent px-4 py-3 text-gray-600 transition duration-300 invalid:ring-2 invalid:ring-red-400 focus:ring-2 focus:ring-cyan-300"
+                      value={email}
+                      required
+                      onChange={(e) => setEmail(e.target.value)}
+                      className="focus:outline-none block w-full rounded-md border border-gray-200 bg-transparent px-4 py-3 text-gray-600 transition duration-300 focus:ring-2 focus:ring-cyan-300"
                     />
                   </div>
 
                   <div>
                     <div className="flex items-center justify-between">
-                      <label
-                        for="pwd"
-                        className="text-gray-600"
-                      >
+                      <label htmlFor="pwd" className="text-gray-600">
                         Password
                       </label>
                     </div>
@@ -48,13 +56,16 @@ export default function Example() {
                       type="password"
                       name="pwd"
                       id="pwd"
-                      autocomplete="current-password"
-                      className="focus:outline-none block w-full rounded-md border border-gray-200 bg-transparent px-4 py-3 text-gray-600 transition duration-300 invalid:ring-2 invalid:ring-red-400 focus:ring-2 focus:ring-cyan-300"
+                      value={password}
+                      required
+                      onChange={(e) => setPassword(e.target.value)}
+                      className="focus:outline-none block w-full rounded-md border border-gray-200 bg-transparent px-4 py-3 text-gray-600 transition duration-300 focus:ring-2 focus:ring-cyan-300"
                     />
                   </div>
 
                   <button
                     type="submit"
+                    onClick={handleSubmit}
                     className="relative flex h-11 w-full items-center justify-center px-6 before:absolute before:inset-0 before:rounded-full before:bg-primary before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95"
                   >
                     <span className="relative text-base font-semibold text-white">
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,13 +1,26 @@
-import NextAuth from 'next-auth'
-import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
-import clientPromise from "../../../lib/mongodb"
+import NextAuth from "next-auth";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import clientPromise from "../../../lib/mongodb";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions = {
-    providers: [],
-    pages: {
-        signIn: '/admin/auth/signin'
-    },
-    adapter: MongoDBAdapter(clientPromise),
-}
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {},
+      async authorize(credentials, req) {
+        try {
+          return null;
+        } catch (err) {
+          throw new Error(err.message);
+        }
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/admin/auth/signin",
+  },
+  adapter: MongoDBAdapter(clientPromise),
+};
 
-export default NextAuth(authOptions)
+export default NextAuth(authOptions);
