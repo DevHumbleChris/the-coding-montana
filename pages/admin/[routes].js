d6211e10325a531caa594ce93310bf0092110ad6
DevHumbleChris <dev.humblechris254@gmail.com>
2022-11-25 17:31:47 +0300

    Admin UI Is Good So Far

--- pages/admin/[routes].js
+++ pages/admin/[routes].js
@@ -36,6 +36,7 @@ export default function Admin() {
   ];
   const router = useRouter();
   const currentPath = router.query.routes;
+  console.log(currentPath);
 
   useEffect(() => {
     const foundComponent = components.find((cmp) => cmp.slug === currentPath);
@@ -72,115 +73,105 @@ export default function Admin() {
 
             <ul className="mt-8 space-y-2 tracking-wide">
               <li>
-                <a
-                  href="#"
-                  aria-label="dashboard"
-                  className="relative flex items-center space-x-4 rounded-xl bg-gradient-to-r from-sky-600 to-cyan-400 px-4 py-3 text-white"
-                >
-                  <svg
-                    className="-ml-1 h-6 w-6"
-                    viewBox="0 0 24 24"
-                    fill="none"
+                <Link href="/admin/dashboard">
+                  <a
+                    aria-label="dashboard"
+                    className={
+                      currentPath === "dashboard"
+                        ? "relative flex items-center space-x-4 rounded-xl bg-gradient-to-r from-sky-600 to-cyan-400 px-4 py-3 text-white"
+                        : "group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600"
+                    }
                   >
-                    <path
-                      d="M6 8a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2V8ZM6 15a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2v-1Z"
-                      className="dark:fill-slate-600 fill-current text-cyan-400"
-                    ></path>
-                    <path
-                      d="M13 8a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2V8Z"
-                      className="fill-current text-cyan-200 group-hover:text-cyan-300"
-                    ></path>
-                    <path
-                      d="M13 15a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2v-1Z"
-                      className="fill-current group-hover:text-sky-300"
-                    ></path>
-                  </svg>
-                  <span className="-mr-1 font-medium">Dashboard</span>
-                </a>
+                    <svg
+                      className="-ml-1 h-6 w-6"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                    >
+                      <path
+                        d="M6 8a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2V8ZM6 15a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2v-1Z"
+                        className="dark:fill-slate-600 fill-current text-cyan-400"
+                      ></path>
+                      <path
+                        d="M13 8a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2V8Z"
+                        className="fill-current text-cyan-200 group-hover:text-cyan-300"
+                      ></path>
+                      <path
+                        d="M13 15a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2v-1Z"
+                        className="fill-current group-hover:text-sky-300"
+                      ></path>
+                    </svg>
+                    <span className="-mr-1 font-medium">Dashboard</span>
+                  </a>
+                </Link>
               </li>
               <li>
-                <a
-                  href="#"
-                  className="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
+                <Link href="/admin/projects">
+                  <a
+                    className={
+                      currentPath === "projects"
+                        ? "relative flex items-center space-x-4 rounded-xl bg-gradient-to-r from-sky-600 to-cyan-400 px-4 py-3 text-white"
+                        : "group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600"
+                    }
                   >
-                    <path
-                      className="fill-current text-gray-300 group-hover:text-cyan-300"
-                      fill-rule="evenodd"
-                      d="M2 6a2 2 0 012-2h4l2 2h4a2 2 0 012 2v1H8a3 3 0 00-3 3v1.5a1.5 1.5 0 01-3 0V6z"
-                      clip-rule="evenodd"
-                    />
-                    <path
-                      className="fill-current text-gray-600 group-hover:text-cyan-600 dark:group-hover:text-sky-400"
-                      d="M6 12a2 2 0 012-2h8a2 2 0 012 2v2a2 2 0 01-2 2H2h2a2 2 0 002-2v-2z"
-                    />
-                  </svg>
-                  <span className="group-hover:text-gray-700 dark:group-hover:text-gray-50">
-                    Categories
-                  </span>
-                </a>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        className="fill-current text-gray-300 group-hover:text-cyan-300"
+                        fillRule="evenodd"
+                        d="M2 6a2 2 0 012-2h4l2 2h4a2 2 0 012 2v1H8a3 3 0 00-3 3v1.5a1.5 1.5 0 01-3 0V6z"
+                        clipRule="evenodd"
+                      />
+                      <path
+                        className="fill-current text-gray-600 group-hover:text-cyan-600"
+                        d="M6 12a2 2 0 012-2h8a2 2 0 012 2v2a2 2 0 01-2 2H2h2a2 2 0 002-2v-2z"
+                      />
+                    </svg>
+                    <span className="group-hover:text-primary">Projects</span>
+                  </a>
+                </Link>
               </li>
               <li>
-                <a
-                  href="#"
-                  className="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600 dark:text-gray-300"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
+                <Link href="/admin/articles">
+                  <a
+                    className={
+                      currentPath === "articles"
+                        ? "relative flex items-center space-x-4 rounded-xl bg-gradient-to-r from-sky-600 to-cyan-400 px-4 py-3 text-white"
+                        : "group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600"
+                    }
                   >
-                    <path
-                      className="fill-current text-gray-600 group-hover:text-cyan-600 dark:group-hover:text-sky-400"
-                      fill-rule="evenodd"
-                      d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z"
-                      clip-rule="evenodd"
-                    />
-                    <path
-                      className="fill-current text-gray-300 group-hover:text-cyan-300"
-                      d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z"
-                    />
-                  </svg>
-                  <span className="group-hover:text-gray-700 dark:group-hover:text-gray-50">
-                    Reports
-                  </span>
-                </a>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        className="fill-current text-gray-600 group-hover:text-cyan-600 dark:group-hover:text-sky-400"
+                        fillRule="evenodd"
+                        d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z"
+                        clipRule="evenodd"
+                      />
+                      <path
+                        className="fill-current text-gray-300 group-hover:text-cyan-300"
+                        d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z"
+                      />
+                    </svg>
+                    <span className="group-hover:text-primary">Articles</span>
+                  </a>
+                </Link>
               </li>
               <li>
+                <Link href="/admin/Finance">
                 <a
-                  href="#"
-                  className="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    <path
-                      className="fill-current text-gray-600 group-hover:text-cyan-600 dark:group-hover:text-cyan-400"
-                      d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"
-                    />
-                    <path
-                      className="fill-current text-gray-300 group-hover:text-cyan-300"
-                      d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"
-                    />
-                  </svg>
-                  <span className="group-hover:text-gray-700 dark:group-hover:text-gray-50">
-                    Other data
-                  </span>
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600 dark:text-gray-300"
+                  className={
+                    currentPath === "articles"
+                      ? "relative flex items-center space-x-4 rounded-xl bg-gradient-to-r from-sky-600 to-cyan-400 px-4 py-3 text-white"
+                      : "group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600"
+                  }
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -194,15 +185,14 @@ export default function Admin() {
                     />
                     <path
                       className="fill-current text-gray-600 group-hover:text-cyan-600 dark:group-hover:text-sky-400"
-                      fill-rule="evenodd"
+                      fillRule="evenodd"
                       d="M18 9H2v5a2 2 0 002 2h12a2 2 0 002-2V9zM4 13a1 1 0 011-1h1a1 1 0 110 2H5a1 1 0 01-1-1zm5-1a1 1 0 100 2h1a1 1 0 100-2H9z"
-                      clip-rule="evenodd"
+                      clipRule="evenodd"
                     />
                   </svg>
-                  <span className="group-hover:text-gray-700 dark:group-hover:text-white">
-                    Finance
-                  </span>
+                  <span className="group-hover:text-primary">Finance</span>
                 </a>
+                </Link>
               </li>
             </ul>
           </div>
@@ -217,9 +207,9 @@ export default function Admin() {
                 stroke="currentColor"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
                 />
               </svg>
@@ -245,9 +235,9 @@ export default function Admin() {
                   stroke="currentColor"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="M4 6h16M4 12h16M4 18h16"
                   />
                 </svg>
@@ -305,9 +295,9 @@ export default function Admin() {
                     stroke="currentColor"
                   >
                     <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
                       d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
                     />
                   </svg>
