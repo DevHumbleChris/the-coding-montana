06924e83e3d9cb27289b48e79e7cd1770cb0998a
DevHumbleChris <dev.humblechris254@gmail.com>
2022-12-07 14:13:43 +0300

    Added contact iD

--- components/Contacts.jsx
+++ components/Contacts.jsx
@@ -42,7 +42,7 @@ export default function Contacts() {
     });
   };
   return (
-    <section className="py-10 bg-gray-100 sm:py-16 lg:py-24">
+    <section className="py-10 bg-gray-100 sm:py-16 lg:py-24" id="contact">
       <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
         <div className="max-w-2xl mx-auto text-center">
           <h2 className="text-3xl font-bold leading-tight text-primary sm:text-4xl lg:text-5xl">
