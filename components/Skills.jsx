57e674c68e9f02cc6f3d0ea7dc7ba14222955ef5
DevHumbleChris <dev.humblechris254@gmail.com>
2023-01-16 16:14:57 +0300

    added firebase svg

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -90,6 +90,35 @@ export default function Skills() {
             <div className="px-2 mx-auto">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
+                width="96"
+                height="96"
+                x="0"
+                y="0"
+                viewBox="0 0 48 48"
+                className="w-12 h-12 mx-auto"
+              >
+                <path
+                  fill="#ff8f00"
+                  d="M8 37L23.234 8.436a.84.84 0 011.494.02L30 19 8 37z"
+                ></path>
+                <path
+                  fill="#ffa000"
+                  d="M8 36.992l5.546-34.199c.145-.895 1.347-1.089 1.767-.285L26 22.992l-18 14z"
+                ></path>
+                <path
+                  fill="#ff6f00"
+                  d="M8.008 36.986l.2-.157 17.529-14.341-4.944-9.476z"
+                ></path>
+                <path
+                  fill="#ffc400"
+                  d="M8 37l26.666-25.713a.954.954 0 011.606.547L40 37l-15 8.743a2.002 2.002 0 01-1.961 0L8 37z"
+                ></path>
+              </svg>
+              <h4 className="text-center text-sm">Firebase</h4>
+            </div>
+            <div className="px-2 mx-auto">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
                 x="0px"
                 y="0px"
                 width="80"
